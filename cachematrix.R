e45a520ff3fb92fa01333c4b1f67ea21f9228ee8
Nirmal Labh <nlabh01@gmail.com>
2014-07-26 20:13:56 -0400

    git commits - R Programming: Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## When a call is made to "makeCacheMatrix", it defines and 
+## returns 4 functions in a list.  Because the variable "x" 
+## exists in the environment where these functions are 
+## defined they have access to its value. These functions 
+## are used in cacheSolve to set and get the matrix
+## and its inverse.
 
-## Write a short comment describing this function
-
+## Function to initialize the matrix
 makeCacheMatrix <- function(x = matrix()) {
+        ## initialize the matrix inverse
+        inv <- NULL
 
+        ## set the mairix and re-initializes the inverse
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+
+        ## gets/returns the matrix
+        get <- function() x
+
+        # calculates the inverse using solve function of R
+        setinverse <- function(solve) inv <<- solve
+
+        ## gets the inverse
+        getinverse <- function() inv
+
+        list(set = set, get = get,
+                setinverse = setinverse,
+                getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## set the matrix; then solve and cache the inverse
+cacheSolve <- function(x) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## retrieve the inverse of 'x'
+        invrs <- x$getinverse()
+
+        ## if inverse exists, return it
+        if(!is.null(invrs)) {
+                message("Getting cached value of inverse....")
+                return(invrs)
+        }
+
+        ## if inverse does not exist yet, calculate and then
+        ## save/cache it
+        mtrx <- x$get()
+        invrs <- solve(mtrx)
+        x$setinverse(invrs)
+        invrs
 }
